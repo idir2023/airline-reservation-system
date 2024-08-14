a507da4019eb6d50985cfbdf5c3fb6c3995341e4
lahcen idir <128367702+idir2023@users.noreply.github.com>
2024-08-14 12:41:39 +0100

    commit new 12.41

--- resources/views/client/visas.blade.php
+++ resources/views/client/visas.blade.php
@@ -4,14 +4,6 @@
 @include('client.layout.head')
 
 <body>
-    <!-- Spinner Start -->
-    <div id="spinner"
-        class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-        <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
-            <span class="visually-hidden">Loading...</span>
-        </div>
-    </div>
-    <!-- Spinner End -->
 
     <!-- Navbar & Hero Start -->
     <div class="container-fluid position-relative p-0">
